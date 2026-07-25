f44ee0aa2b1661b1a648344e1fac9e986ad43172
KENRASLAN <164725417+KENRASLAN@users.noreply.github.com>
2026-07-25 13:55:25 -0700

    Create config.js

---
+++ Frontend/config.js
@@ -0,0 +1,15 @@
+// Firebase Configuration
+const firebaseConfig = {
+ apiKey: "AIzaSyAnVeaSbAQrfsLsp18rL63slM1lpW_84aY",
+  authDomain: "krea2-fa32c.firebaseapp.com",
+  projectId: "krea2-fa32c",
+  storageBucket: "krea2-fa32c.firebasestorage.app",
+  messagingSenderId: "50504153178",
+  appId: "1:50504153178:web:2aa0815b1e2fb38c5c0063"
+};
+
+// Backend API URL (Update this when Kaggle session changes)
+const API_CONFIG = {
+    baseUrl: "https://reactive-unnamed-porthole.ngrok-free.dev", // Will update this
+    timeout: 120000 // 2 minutes timeout for generation
+};
